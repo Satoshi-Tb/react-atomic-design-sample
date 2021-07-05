6de618c4ed0e78b2aa1da7ae4cd7dc16b8159144
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2021-07-06 00:43:48 +0900

    Cardコンポーネント追加(molecules, organisms)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+const user = {
+  name: "吾輩",
+  image: "https://source.unsplash.com/FilM6ng7VGQ",
+  email: "12345@hoge.foo",
+  phone: "000-1234-5678",
+  company: {
+    name: "株式会社猫である"
+  },
+  website: "https://google.co.jp"
+};
 
 export default function App() {
   return (
@@ -10,6 +21,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
