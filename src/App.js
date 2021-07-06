4135c95094d128f20a2bc4dfafe2ab8e13fd8a99
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2021-07-07 00:10:51 +0900

    Add Pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
 import "./styles.css";
-const user = {
-  name: "吾輩",
-  image: "https://source.unsplash.com/FilM6ng7VGQ",
-  email: "12345@hoge.foo",
-  phone: "000-1234-5678",
-  company: {
-    name: "株式会社猫である"
-  },
-  website: "https://google.co.jp"
-};
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
+      <Router />
     </BrowserRouter>
   );
 }
